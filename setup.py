02fb1bbb095aa8b5a597aa79d4845e6596030178
Maurits van Rees <maurits@vanrees.org>
2015-06-08 10:21:28 +0200

    setup.py: more classifiers and fix link.
    I guess it may still work on Plone 3, but I am not going to claim this here.

--- setup.py
+++ setup.py
@@ -12,12 +12,18 @@ setup(name='collective.portlet.relateditems',
       version=version,
       description="A Plone portlet to display similar items to context",
       long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
           "Framework :: Plone",
           "Framework :: Zope2",
           "Framework :: Zope3",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
           ],
       keywords='related, categories, keywords',
